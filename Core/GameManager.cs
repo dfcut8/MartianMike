c9b2c901f16dd36f472c113824337d48ccd399e1
dfcut8 <dfcut8@proton.me>
2025-09-20 17:55:03 -0400

    basic setup for resetting level and quiting

--- Core/GameManager.cs
+++ Core/GameManager.cs
@@ -1,6 +1,18 @@
 using Godot;
-using System;
 
 public partial class GameManager : Node
 {
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("global_reset_level"))
+        {
+            GD.Print("Reloading current level...");
+            GetTree().ReloadCurrentScene();
+        }
+        if (Input.IsActionJustPressed("global_quit"))
+        {
+            GD.Print("Quitting the game...");
+            GetTree().Quit();
+        }
+    }
 }
