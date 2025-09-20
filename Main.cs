f23cbf3e32c6ab7a2c7330168f71fd9713d11082
dfcut8 <dfcut8@proton.me>
2025-09-20 17:38:17 -0400

    basic main menu

---
+++ Main.cs
@@ -0,0 +1,32 @@
+using Godot;
+
+public partial class Main : Control
+{
+    private Button startButton;
+    private Button exitButton;
+
+    public override void _Ready()
+    {
+        startButton = GetNode<Button>("%Start");
+        exitButton = GetNode<Button>("%Exit");
+
+        startButton.Pressed += OnStartButtonPressed;
+        exitButton.Pressed += OnExitButtonPressed;
+    }
+
+    private void OnExitButtonPressed()
+    {
+        GetTree().Quit();
+    }
+
+    private void OnStartButtonPressed()
+    {
+        GetTree().ChangeSceneToFile("Levels/Level1.tscn");
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        startButton.Pressed -= OnStartButtonPressed;
+        exitButton.Pressed -= OnExitButtonPressed;
+    }
+}
