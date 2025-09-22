258037a2d5c92cfd3caef3e9f57a1b0ee8761e79
dfcut8 <dfcut8@proton.me>
2025-09-22 10:57:13 -0400

    add physics and basic animations

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,6 +3,8 @@ using Godot;
 public partial class Player : CharacterBody2D
 {
     [Export] private int gravity = 400;
+    [Export] private int speed = 150;
+    [Export] private int jumpVelocity = -200;
     private AnimatedSprite2D animatedSprite2D;
 
     public override void _Ready()
@@ -12,43 +14,51 @@ public partial class Player : CharacterBody2D
 
     public override void _PhysicsProcess(double delta)
     {
-        HandlePlayerInputs();
-        AddGravity(delta);
+        var velocity = Velocity;
+        HandlePlayerInputs(ref velocity);
+        AddGravity(delta, ref velocity);
+        Velocity = velocity;
         MoveAndSlide();
     }
 
-    private void AddGravity(double delta)
+    private void AddGravity(double delta, ref Vector2 v)
     {
-        var velocity = Velocity;
         if (!IsOnFloor())
         {
-            velocity.Y += gravity * (float)delta;
-            Velocity = velocity;
+            animatedSprite2D.Play("fall");
+            v.Y += gravity * (float)delta;
         }
     }
 
-    private void HandlePlayerInputs()
+    private void HandlePlayerInputs(ref Vector2 v)
     {
         if (Input.IsActionPressed("ui_right"))
         {
-            //Velocity.X = 100;
-            animatedSprite2D.Play("run");
+            v.X = speed;
+            if (IsOnFloor())
+            {
+                animatedSprite2D.Play("run");
+            }
             animatedSprite2D.FlipH = false;
         }
         else if (Input.IsActionPressed("player_left"))
         {
-            //Velocity.X = -100;
-            animatedSprite2D.Play("run");
+            v.X = -speed;
+            if (IsOnFloor())
+            {
+                animatedSprite2D.Play("run");
+            }
             animatedSprite2D.FlipH = true;
         }
         else
         {
-            //Velocity.X = 0;
+            v.X = 0;
             animatedSprite2D.Play("idle");
         }
 
         if (Input.IsActionJustPressed("player_jump"))
         {
+            v.Y = jumpVelocity;
             animatedSprite2D.Play("jump");
         }
     }
