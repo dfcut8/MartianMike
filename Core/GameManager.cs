965d28877edbf6ef0616c5e6f2b6ae8be846eeae
dfcut8 <dfcut8@proton.me>
2025-09-23 08:04:00 -0400

    refactor to use game manager for all deaths

--- Core/GameManager.cs
+++ Core/GameManager.cs
@@ -2,6 +2,22 @@ using Godot;
 
 public partial class GameManager : Node
 {
+    private Marker2D playerRespawn;
+    private Area2D deathZone;
+
+    public override void _Ready()
+    {
+        playerRespawn = GetNode<Marker2D>("%PlayerRespawn");
+        deathZone = GetNode<Area2D>("%DeathZone");
+        deathZone.BodyEntered += body =>
+        {
+            if (body is Player)
+            {
+                body.Position = playerRespawn.Position;
+            }
+        };
+    }
+
     public override void _Process(double delta)
     {
         if (Input.IsActionJustPressed("global_reset_level"))
