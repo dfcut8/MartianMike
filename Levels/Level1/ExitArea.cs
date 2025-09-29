a41b88f5353db7c20c313a637e6e0f60a1ff29cb
dfcut8 <dfcut8@proton.me>
2025-09-29 11:30:44 -0400

    add exit area to the level

---
+++ Levels/Level1/ExitArea.cs
@@ -0,0 +1,27 @@
+using Godot;
+
+using MartianMike.Actors;
+
+public partial class ExitArea : Area2D
+{
+    private AnimatedSprite2D animatedSprite2D;
+    public override void _Ready()
+    {
+        animatedSprite2D = GetNode<AnimatedSprite2D>("%AnimatedSprite2D");
+        animatedSprite2D.Play("idle");
+        BodyEntered += OnBodyEntered;
+    }
+
+    private void OnBodyEntered(Node body)
+    {
+        if (body is Player)
+        {
+            animatedSprite2D.Play("pressed");
+        }
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        BodyEntered -= OnBodyEntered;
+    }
+}
