68168592d144b655ab833e6c21ca5414aa0b4a48
dfcut8 <dfcut8@proton.me>
2025-10-02 09:37:49 -0400

    add handler for background music

--- Audio/AudioManager.cs
+++ Audio/AudioManager.cs
@@ -1,6 +1,15 @@
 using Godot;
-using System;
 
 public partial class AudioManager : Node
 {
+    private AudioStreamPlayer backgroundMusicPlayer;
+
+    public override void _Ready()
+    {
+        backgroundMusicPlayer = GetNode<AudioStreamPlayer>("%BackgroundMusicPlayer");
+
+        // TODO: Make volume adjustable in settings menu
+        backgroundMusicPlayer.VolumeDb = -10;
+        backgroundMusicPlayer.Play();
+    }
 }
