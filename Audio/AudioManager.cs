003a008fd32f8c51893df8127cf04947b5eb4b09
dfcut8 <dfcut8@proton.me>
2025-10-02 09:57:59 -0400

    add additional sfx player

--- Audio/AudioManager.cs
+++ Audio/AudioManager.cs
@@ -3,10 +3,44 @@ using Godot;
 public partial class AudioManager : Node
 {
     private AudioStreamPlayer backgroundMusicPlayer;
+    private AudioStreamPlayer soundEffectPlayer;
+
+    private static AudioManager? instance;
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (instance is null)
+                GD.PushWarning("AudioManager instance is not initialized yet.");
+            return instance!;
+        }
+    }
 
     public override void _Ready()
     {
+        if (instance != null && instance != this)
+        {
+            QueueFree();
+            return;
+        }
+        instance = this;
+
         backgroundMusicPlayer = GetNode<AudioStreamPlayer>("%BackgroundMusicPlayer");
+        soundEffectPlayer = GetNode<AudioStreamPlayer>("%SoundEffectPlayer");
+
+        PlayBackgroundMusic();
+    }
+
+    public void PlaySoundEffect(AudioStream soundEffect, float volumeDb = 0f)
+    {
+        soundEffectPlayer.Stream = soundEffect;
+        soundEffectPlayer.VolumeDb = volumeDb;
+        soundEffectPlayer.Play();
+
+    }
+
+    private void PlayBackgroundMusic()
+    {
         backgroundMusicPlayer.ProcessMode = ProcessModeEnum.Always;
 
         // TODO: Make volume adjustable in settings menu
